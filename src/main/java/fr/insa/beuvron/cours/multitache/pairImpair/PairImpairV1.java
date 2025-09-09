7b7ad13156d0232b6a9dde8a838ceffebd733bec
fdb <fdb@mail.com>
2025-09-09 09:12:09 +0200

    v21

--- src/main/java/fr/insa/beuvron/cours/multitache/pairImpair/PairImpairV1.java
+++ src/main/java/fr/insa/beuvron/cours/multitache/pairImpair/PairImpairV1.java
@@ -24,4 +24,10 @@ package fr.insa.beuvron.cours.multitache.pairImpair;
  */
 public class PairImpairV1 {
     
+    public static void main(String[] args) {
+        for (int i = 0 ; i < 10 ; i ++) {
+            System.out.println(i);
+        }
+    }
+    
 }
